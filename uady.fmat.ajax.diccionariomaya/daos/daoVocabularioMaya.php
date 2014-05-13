0f33243bedcacc4e8a61c684311d763d7e9eab40
ileanaontiveros <ontiveros.ig@hushmail.com>
2014-05-12 22:30:55 -0500

    Ajustes en la funcionalidad Agregar Categoría. Se actualizó el DAO de vocabulario.

--- uady.fmat.ajax.diccionariomaya/daos/daoVocabularioMaya.php
+++ uady.fmat.ajax.diccionariomaya/daos/daoVocabularioMaya.php
@@ -7,37 +7,38 @@ ORM::configure('username', 'root');
 ORM::configure('password', 'centenario');
 
 function guardarVocabularioMaya($textoMaya, $rutaAudio, $categoriaId) {
+    
+    try {
+    
+        $nueva_entrada_maya = Model::factory('VocabularioMaya')->create();
+        $nueva_entrada_maya->texto_maya = $textoMaya;
+        $nueva_entrada_maya->nombre_audio = $rutaAudio;
+        $nueva_entrada_maya->categoria_id = $categoriaId;
+        $nueva_entrada_maya->save();
 
-  try {
-  	$nueva_entrada_maya = Model::factory('VocabularioMaya')->create();
-    $nueva_entrada_maya->texto_maya = $textoMaya;
-    $nueva_entrada_maya->nombre_audio = $rutaAudio;
-    $nueva_entrada_maya->categoria_id = $categoriaId;
-    $nueva_entrada_maya->save();
+        return $nueva_entrada_maya;
 
-    return $nueva_entrada_maya;
-
-  } catch (Exception $e) {
+    } catch (Exception $e) {
   	return false;
-  }
+    }
 
 }
 
 function agregarTraduccionEspaniol($palabra, $idEspaniol){
-
- // try {
+    
+    try {
   	$es_traduccion_de = Model::factory('VocabularioMayaVocabularioEspaniol')->create();
     
-    $es_traduccion_de->espaniol_id = $idEspaniol;
+        $es_traduccion_de->espaniol_id = $idEspaniol;
 
-    $es_traduccion_de->maya_id = $palabra->maya_id;
-    $es_traduccion_de->save(); 
+        $es_traduccion_de->maya_id = $palabra->maya_id;
+        $es_traduccion_de->save(); 
 
-    return true;
-
-  //} catch (Exception $e) {
-  //	return false;
-  //}
+        return true;
+        
+    } catch (Exception $e) {
+  	return false;
+    }
 
 }
 
